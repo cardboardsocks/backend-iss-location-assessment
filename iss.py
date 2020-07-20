df2d7c013fdd9a9e007ae389aea3182c4dc0cdb0
Ben McKenzie <bmckenzie0522@gmail.com>
2020-07-20 15:20:56 -0400

    a bit

--- iss.py
+++ iss.py
@@ -14,31 +14,31 @@ def get_coords():
     r = requests.get('http://api.open-notify.org/iss-now.json')
     return r.json()
 
-def show_next_pass():
+def get_pass():
     r = requests.get('http://api.open-notify.org/iss-pass.json',params={'lat': '39.7684', 'lon': '-86.1581'})
     return r.json()
-    print(show_next_pass)
+    print(get_pass)
 
 def iss_module():
     iss = turtle.Turtle()
-    next_pass = show_next_pass()
-    rise_time = time.ctime(next_pass["response"][0]["risetime"])
+    n_pass = get_pass()
+    rise_time = time.ctime(n_pass["response"][0]["risetime"])
     display = turtle.Screen()
     display.setup(width=720, height=360, startx=0, starty=0)
     display.setworldcoordinates(-180, -90, 180, 90)
     display.bgpic('map.gif')
     display.register_shape('iss.gif')
     iss.shape("iss.gif")
+    iss.dot(4, "red")
+    iss.color('red')
     iss.penup()
     iss.goto(-86.1581, 39.7684)
-    iss.dot(5, "red")
-    iss.color('red')
     iss.write(rise_time, align='right', font=("Times New Roman", 16))
     return iss
     
 def main():
     info = astros()
-    print(f"Number of astronauts on the {info['people'][0]['craft']}: {info['number']}")
+    print(f"Astronauts on the {info['people'][0]['craft']}: {info['number']}")
     for astro in info["people"]:
         print(f"- {astro['name']}")
     iss = iss_module()
